1eccb5b2ca8218e964471a88616a1d875e3b1ee0
Mowmaster <mowmaster@outlook.com>
2023-08-13 02:22:10 -0500

    started repo and added loot handler and mob chunk

---
+++ src/main/java/com/mowmaster/moblootblock/EventHandlers/LootModifier.java
@@ -0,0 +1,81 @@
+package com.mowmaster.moblootblock.EventHandlers;
+
+import com.mowmaster.moblootblock.DeferredRegistries.DeferredRegisterItems;
+import com.mowmaster.moblootblock.moblootblock;
+import com.mowmaster.mowlib.MowLibUtils.MowLibCompoundTagUtils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.entity.EntityAccess;
+import net.minecraftforge.common.extensions.IForgeEntity;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mod.EventBusSubscriber
+public class LootModifier {
+
+    //https://github.com/Mrbysco/ForceCraft/blob/1.20/src/main/java/com/mrbysco/forcecraft/handlers/LootingHandler.java
+    @SubscribeEvent()
+    public static void MobChunkMobLoot(LivingDropsEvent event)
+    {
+        if (event.getSource() == null || event.getSource().getEntity() == null) {
+            return;
+        }
+
+        Entity source = event.getSource().getEntity();
+        if (source instanceof Player player) {
+
+            int hasEnchant = player.getMainHandItem().getEnchantmentLevel(Enchantments.BINDING_CURSE);
+            if(hasEnchant>0)
+            {
+                RandomSource rand = player.getRandom();
+                int looting = event.getLootingLevel();
+                LivingEntity entity = event.getEntity();
+                BlockPos entityPos = entity.blockPosition();
+                Level entityLevel = entity.level();
+                ResourceLocation entityLoc = ForgeRegistries.ENTITY_TYPES.getKey(entity.getType());
+
+                int chanceMax = 20;
+                if (looting > 0) {
+                    chanceMax = chanceMax / looting;
+                    if (chanceMax < 0) {
+                        chanceMax = 1;
+                    }
+                }
+                float dropChance = rand.nextInt(chanceMax);
+                if (dropChance == 0) {
+                    //https://github.com/CreeperHost/SoulShards/blob/1.20/src/main/java/net/creeperhost/soulshardsrespawn/core/EventHandler.java
+                    ItemStack getDroppedChunk = new ItemStack(DeferredRegisterItems.MOB_BIT.get(), rand.nextInt(Math.max(1, looting)) + 1);
+                    getDroppedChunk.setTag(MowLibCompoundTagUtils.writeStringToNBT(moblootblock.MODID,getDroppedChunk.getOrCreateTag(),entityLoc.toString(),"moblootitem"));
+                //MowLibCompoundTagUtils.readStringFromNBT(moblootblock.MODID,p_41421_.getOrCreateTag(),"moblootitem")
+
+                    if (entity.isInvertedHealAndHarm()) {
+                        event.getDrops().add(new ItemEntity(entityLevel, entityPos.getX(), entityPos.getY(), entityPos.getZ(), getDroppedChunk));
+                    } else {
+                        if (entity instanceof Monster) {
+                            event.getDrops().add(new ItemEntity(entityLevel, entityPos.getX(), entityPos.getY(), entityPos.getZ(), getDroppedChunk));
+                        } else {
+                            event.getDrops().add(new ItemEntity(entityLevel, entityPos.getX(), entityPos.getY(), entityPos.getZ(), getDroppedChunk));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/mowmaster/moblootblock/Items/MobLootItem.java
@@ -0,0 +1,31 @@
+package com.mowmaster.moblootblock.Items;
+
+import com.mowmaster.moblootblock.moblootblock;
+import com.mowmaster.mowlib.MowLibUtils.MowLibCompoundTagUtils;
+import com.mowmaster.mowlib.MowLibUtils.MowLibTooltipUtils;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class MobLootItem extends Item
+{
+    public MobLootItem(Properties p_41383_) {
+        super(p_41383_);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack p_41421_, @Nullable Level p_41422_, List<Component> p_41423_, TooltipFlag p_41424_) {
+        super.appendHoverText(p_41421_, p_41422_, p_41423_, p_41424_);
+        MutableComponent component = Component.translatable(moblootblock.MODID + ".moblootitem.tooltip");
+        MutableComponent mob = EntityType.byString(MowLibCompoundTagUtils.readStringFromNBT(moblootblock.MODID,p_41421_.getOrCreateTag(),"moblootitem")).get().getDescription().copy();
+        component.append(mob);
+        MowLibTooltipUtils.addTooltipMessage(p_41423_,p_41421_,component);
+    }
+}
